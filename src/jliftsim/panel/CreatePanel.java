120897dbe2980e3455cc228049159e697069b8b3
Bernard Ladenthin <bernard@ladenthin.net>
2012-06-06 16:24:13 +0200

    implement JSeperator for a better creation view

--- src/jliftsim/panel/CreatePanel.java
+++ src/jliftsim/panel/CreatePanel.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import jliftsim.frame.SimFrame;
 
 /**
@@ -21,6 +22,9 @@ public class CreatePanel extends JPanel implements ActionListener {
 
     private JButton m_JButtonCreateInstance;
 
+    private JSeparator m_FloorsSeparator;
+    private JSeparator m_EngineSeparator;
+
     private List<SimFrame> m_SimFrames;
 
     public CreatePanel() {
@@ -31,29 +35,43 @@ public class CreatePanel extends JPanel implements ActionListener {
     private void createAndShowGUI() {
         this.setLayout(null);
         Dimension dm = new Dimension();
-        dm.setSize(300, 230);
+        dm.setSize(300, 238);
         this.setPreferredSize(dm);
 
         /*
          * Common size
          */
         Rectangle r = new Rectangle();
+        Rectangle rFloorsSeparator = new Rectangle();
+        Rectangle rEngineSeparator = new Rectangle();
 
         m_CreateFloorsPanel = new CreateFloorsPanel();
         r.x = 0;
         r.y = 0;
         r.width = m_CreateFloorsPanel.getPreferredSize().width;
         r.height = m_CreateFloorsPanel.getPreferredSize().height;
+
+        rFloorsSeparator.x = 0;
+        rFloorsSeparator.y = r.y + r.height;
+        rFloorsSeparator.height = 2;
+        rFloorsSeparator.width = r.width;
+        
         m_CreateFloorsPanel.setBounds(r);
-        m_CreateFloorsPanel.setBackground(java.awt.Color.BLUE);
+        //m_CreateFloorsPanel.setBackground(java.awt.Color.BLUE);
         this.add(m_CreateFloorsPanel);
 
         m_CreateEnginePanel = new CreateEnginePanel();
-        r.y += r.height;
+        r.y += r.height + rFloorsSeparator.height;
         r.width = m_CreateEnginePanel.getPreferredSize().width;
         r.height = m_CreateEnginePanel.getPreferredSize().height;
+
+        rEngineSeparator.x = 0;
+        rEngineSeparator.y = r.y + r.height;
+        rEngineSeparator.height = 2;
+        rEngineSeparator.width = r.width;
+
         m_CreateEnginePanel.setBounds(r);
-        m_CreateEnginePanel.setBackground(java.awt.Color.YELLOW);
+        //m_CreateEnginePanel.setBackground(java.awt.Color.YELLOW);
         this.add(m_CreateEnginePanel);
 
         /*
@@ -61,13 +79,25 @@ public class CreatePanel extends JPanel implements ActionListener {
          */
         m_JButtonCreateInstance = new JButton();
         r.x = 10;
-        r.y += r.height;
+        r.y += r.height + rEngineSeparator.height + 4;
         r.width = 170;
         r.height = 20;
         m_JButtonCreateInstance.setText("Create lift simulation");
         m_JButtonCreateInstance.setBounds(r);
         m_JButtonCreateInstance.addActionListener(this);
         this.add(m_JButtonCreateInstance);
+
+        //######################################################################
+        /*
+         * Separator
+         */
+        m_FloorsSeparator = new JSeparator();
+        m_FloorsSeparator.setBounds(rFloorsSeparator);
+        this.add(m_FloorsSeparator);
+
+        m_EngineSeparator = new JSeparator();
+        m_EngineSeparator.setBounds(rEngineSeparator);
+        this.add(m_EngineSeparator);
     }
 
     @Override
